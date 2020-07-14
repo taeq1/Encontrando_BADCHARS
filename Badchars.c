b008c29ee0b3d86b5c9b03817e10629bc4c8e810
taeq1 <52249818+taeq1@users.noreply.github.com>
2020-07-14 12:49:31 -0300

    Update Badchars.c

--- Badchars.c
+++ Badchars.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	fwrite(lixo,1,1012,arquivo);
 	fputs(ret,arquivo);
-	for(i=0;i<=0XFFF;i++)
+	for(i=0;i<=0XFF;i++)
 	{
 		if(i == 0x0) continue; //if(i == 0x0a) continue; e assim sucessivamente..
 		fwrite(&i,1,1,arquivo);
